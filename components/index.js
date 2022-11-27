92b3d696b2ae72d40be186baa01e40cf2866a534
Thanathip <dannajaeiei@hotmail.com>
2022-11-27 18:38:11 +0700

    feat: Export card list and select component

--- components/index.js
+++ components/index.js
@@ -1,2 +1,4 @@
 export { default as Navbar } from './Navbar'
 export { default as Chart } from './Chart'
+export { default as CardList } from './CardList'
+export { default as Select } from './Select'
