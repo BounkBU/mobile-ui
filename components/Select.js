f4c87a5ccc778afce414913ab5b22e36f972cd16
Thanathip <dannajaeiei@hotmail.com>
2022-11-27 18:37:56 +0700

    feat: Added select option component

---
+++ components/Select.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native'
+import SelectDropdown from 'react-native-select-dropdown'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+
+export default function Select({ options, setAction }) {
+  return (
+    <SelectDropdown
+      data={options}
+      onSelect={(_, index) => {
+        setAction(index + 1)
+      }}
+      defaultButtonText={'Select data'}
+      buttonStyle={styles.dropdown1BtnStyle}
+      buttonTextStyle={styles.dropdown1BtnTxtStyle}
+      renderDropdownIcon={(isOpened) => {
+        return (
+          <FontAwesome
+            name={isOpened ? 'chevron-up' : 'chevron-down'}
+            color={'#444'}
+            size={18}
+          />
+        )
+      }}
+      dropdownIconPosition={'right'}
+      dropdownStyle={styles.dropdown1DropdownStyle}
+      rowStyle={styles.dropdown1RowStyle}
+      rowTextStyle={styles.dropdown1RowTxtStyle}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  dropdown1BtnStyle: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#444',
+  },
+  dropdown1BtnTxtStyle: { color: '#444', textAlign: 'left' },
+  dropdown1DropdownStyle: { backgroundColor: '#EFEFEF' },
+  dropdown1RowStyle: {
+    backgroundColor: '#EFEFEF',
+    borderBottomColor: '#C5C5C5',
+  },
+  dropdown1RowTxtStyle: { color: '#444', textAlign: 'left' },
+})
