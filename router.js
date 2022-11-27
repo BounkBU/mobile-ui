e465045a8144336e0dc4f3c84e4bae4ef9a00018
Thanathip <dannajaeiei@hotmail.com>
2022-11-28 03:57:49 +0700

    fix: Remove app state

--- router.js
+++ router.js
@@ -1,11 +1,8 @@
-import { useSelector } from 'react-redux'
 import { Route, Routes } from 'react-router-native'
 import { Home, Visualization } from './pages'
 import Information from './pages/Information'
 
 export default function Router() {
-  const app = useSelector((state) => state.app)
-
   return (
     <Routes>
       <Route path='/' element={<Home />} />
